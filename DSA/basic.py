427a7b30d0576909ba5ce1e6667029436f38717d
neelakandan <neelakandan2001kpm@gmail.com>
2024-02-17 23:32:52 +0530

    django

---
+++ DSA/basic.py
@@ -0,0 +1,14 @@
+# l=[1,2,3,4]
+# a=4
+# for i in l:
+#     if i==a:
+#         print('true')
+#     else:
+#         print(i)
+
+f=[1,8,7,5,5]
+l=[]
+for i in f:
+    if i not in l:
+        l.append(i)
+print(l)
